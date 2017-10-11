8a29855f1a8b10427895c40c346ed1d90c7b8e0c
Gyunay Muradov <GYUNAY@Gyunays-MacBook-Pro.local>
2017-10-11 10:00:12 +0300

    added getters and setters to the class and an empty constructor

--- model/Comment.php
+++ model/Comment.php
@@ -1,15 +1,119 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: GYUNAY
- * Date: 10/11/17
- * Time: 8:21 AM
- */
 
 namespace model;
 
 
-class Comment
-{
+class Comment {
+    private $id;
+    private $videoId;
+    private $userId;
+    private $text;
+    private $dateAdded;
+    private $creatorUsername;
+
+    /**
+     * Comment constructor.
+     */
+    public function __construct() {
+
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVideoId()
+    {
+        return $this->videoId;
+    }
+
+    /**
+     * @param mixed $videoId
+     */
+    public function setVideoId($videoId)
+    {
+        $this->videoId = $videoId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param mixed $userId
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param mixed $text
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateAdded()
+    {
+        return $this->dateAdded;
+    }
+
+    /**
+     * @param mixed $dateAdded
+     */
+    public function setDateAdded($dateAdded)
+    {
+        $this->dateAdded = $dateAdded;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatorUsername()
+    {
+        return $this->creatorUsername;
+    }
+
+    /**
+     * @param mixed $creatorUsername
+     */
+    public function setCreatorUsername($creatorUsername)
+    {
+        $this->creatorUsername = $creatorUsername;
+    }
+
 
 }

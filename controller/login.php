9990a30cefc577f6fbe4c4453ff0634f227bbb02
Gyunay Muradov <GYUNAY@Gyunays-MacBook-Pro.local>
2017-10-11 21:11:58 +0300

    created controller folder with login and register controllers inside for testing purposes

---
+++ controller/login.php
@@ -0,0 +1,30 @@
+<?php
+
+use model\db\UserDao;
+use model\User;
+
+session_start();
+
+function __autoload($className) {
+    $className = '..\\' . $className;
+    $className = str_replace("\\", "/", $className);
+    require_once $className . '.php';
+}
+
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $userModel = UserDao::getInstance();
+    $user = new User();
+    $user->setUsername($username);
+    $user->setPassword($password);
+    $result = $userModel->loginUser($user);
+    if ($result === false) {
+        echo "Invalid username or password.";
+        require_once '../view/login.html';
+    } else {
+        $_SESSION['userId'] = $result->getId();
+        header("Location:../view/index.php");
+    }
+}
---
+++ controller/register.php
@@ -0,0 +1,38 @@
+<?php
+
+use model\db\UserDao;
+use model\User;
+
+session_start();
+
+function __autoload($className) {
+    $className = '..\\' . $className;
+    $className = str_replace("\\", "/", $className);
+    require_once $className . '.php';
+}
+
+if (isset($_POST['register'])) {
+    $userModel = UserDao::getInstance();
+    $user = new User();
+    $user->setFirstName($_POST['firstName']);
+    $user->setLastName($_POST['lastName']);
+    $user->setEmail($_POST['email']);
+    $user->setUsername($_POST['username']);
+    $user->setPassword($_POST['password']);
+    if (isset($_FILES['photo'])) {
+        $name = basename($_FILES["photo"]["name"]);
+        move_uploaded_file($_FILES['photo']['tmp_name'], '../uploads/' . $name);
+        $user->setUserPhotoUrl('/uploads/' . $name);
+    } else {
+        $user->setUserPhotoUrl('/uploads/default_photo.png');
+    }
+
+    $success = $userModel->insertUser($user);
+    if ($success) {
+        header('Location:../view/index.php');
+    } else {
+        echo "Registration was unsuccessful. Try again.";
+        require_once '../view/register.html';
+    }
+
+}

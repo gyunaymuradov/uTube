15907cdbaeb3d7ba923b3b28e144822d33f4011d
unknown <alx_mar@abv.bg>
2017-10-14 19:10:18 +0300

    fixed profile.js relative path

--- view/components/header.php
+++ view/components/header.php
@@ -31,7 +31,7 @@ if (isset($_SESSION['user'])) {
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <script src="http://localhost/uTube/assets/js/profile.js"></script>
+        <script src="../assets/js/profile.js"></script>
         <link rel="stylesheet" href="../assets/style/style.css" type="text/css">
         <title>uTube</title>
     </head>

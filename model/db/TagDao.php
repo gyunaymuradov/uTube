a7d7e46dbd93d8862e1a4a9fbafe01ee3ba7fc8f
Gyunay Muradov <gmuradov93@gmail.com>
2017-10-16 11:50:47 +0300

    Added function to get all tags

--- model/db/TagDao.php
+++ model/db/TagDao.php
@@ -2,30 +2,43 @@
 namespace model\db;
 use model\db\DBManager;
 use \PDO;
-class TagDao {
+class TagDao
+{
     private static $instance;
     private $pdo;
     const GET_TAGS_BY_ID = "SELECT name FROM tags WHERE id=?";
-    private function __construct() {
+    const GET_ALL = "SELECT name FROM tags";
+
+    private function __construct()
+    {
         $this->pdo = DBManager::getInstance()->dbConnect();
     }
-    public static function getInstance() {
+
+    public static function getInstance()
+    {
         if (self::$instance === null) {
             self::$instance = new TagDao();
         }
         return self::$instance;
     }
+
     /**
      * Returns name of a tag by given id
      * @param int $tagID
      * @return mixed
      */
-    function getTagNameById($tagID) {
+    function getTagNameById($tagID)
+    {
         $statement = $this->pdo->prepare(self::GET_TAGS_BY_ID);
         $statement->execute(array($tagID));
         $result = $statement->fetch(PDO::FETCH_ASSOC);
         return $result['name'];
     }
-    //tested - works fine
-    //may need to add functions for adding new tags
+
+    function getAll() {
+        $statement = $this->pdo->prepare(self::GET_ALL);
+        $statement->execute();
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
 }

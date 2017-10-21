a8f3e0bed1f9e2ca446541e1fc0ff7a1ec981224
unknown <alx_mar@abv.bg>
2017-10-22 00:10:10 +0300

    Added forgotten error page. Added different website logo choices for further design changes

---
+++ view/index/error.php
@@ -0,0 +1,7 @@
+<div class="col-md-10 justify-content-center text-center">
+    <h1>OH NOOO!!!</h1>
+    <h4>Our website! It went SKRA!!! PA PA PA!!</h4>
+    <p>Seriously though, sorry about that, we are working on fixing it! Thank you for your patience :)</p>
+    <p>Here, enjoy a video:</p>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/KZgjdQwuUb8" frameborder="0" allowfullscreen></iframe>
+</div>
